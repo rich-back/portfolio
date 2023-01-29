219c12a6e802dbc24ee7382a3842e1218b01546e
rich-back <backhouse.richard@gmail.com>
2023-01-29 23:06:07 +0000

    Light Dark mode switch added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import './App.css';
+import { createContext, useState } from 'react';
+import MainContainer from './containers/MainContainer';
+import ReactSwitch from 'react-switch';
+
+export const ThemeContext = createContext(null);
+
 function App() {
+  const [theme, setTheme] = useState("light")
+
+  const toggleTheme = () => {
+    setTheme((current) => (current === "light" ? "dark" : "light"));
+  }
 
   return (
-    <div className="App">
+    <ThemeContext.Provider value={{ toggleTheme }}>
+    <div className="App" id={theme}>
       <h1>Welcome to my Portfolio!</h1>
+      <div className='switch'>
+      <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}/>
+      <label> {theme === "light" ? "Light Mode" : "Dark Mode"} </label>
+      </div>
+      <MainContainer theme={theme}/>
     </div>
+    </ThemeContext.Provider>
   );
 }
 
---
+++ src/containers/MainContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MainContainer = ({ theme }) => {
+    return (
+        <div className="main-container" id={theme}>
+            ***UNDER CONSTRUCTION*** <br></br>
+            <h1>HERE BE THE MAIN CONTAINER</h1><br></br>
+            ***UNDER CONSTRUCTION***
+        </div>
+    )
+}
+
+export default MainContainer;
